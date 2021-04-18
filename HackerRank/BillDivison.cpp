39e2b4effecfd34761628049ce9597e2d53d93ea
Mouradouchane <mouradouchane@gmail.com>
2021-04-18 01:28:16 +0000

    new soultion - Bill Divison

---
+++ HackerRank/BillDivison.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+void bonAppetit(vector<int> bill, int k, int b) {
+    int annaTotal = 0;
+    int total = 0;
+
+    for(int i = 0 ; i < bill.size() ; i += 1){
+        if(i == k) continue;
+        else{
+            total+= bill[i];
+        }
+    }
+
+    annaTotal = total / 2;
+
+    if(annaTotal == b) std::cout << "Bon Appetit" << std::endl;
+    else std::cout << b - annaTotal << std::endl;
+}
+
