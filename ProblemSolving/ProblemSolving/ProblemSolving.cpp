1f002d5398410a414491b7685ba507d680ebd5d6
Mourad ouchane <mouradouchane@gmail.com>
2021-05-03 22:49:03 +0000

    solving Jumping On The Clouds II problem O(n-1)

--- ProblemSolving/ProblemSolving/ProblemSolving.cpp
+++ ProblemSolving/ProblemSolving/ProblemSolving.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <map>
 
+using namespace std;
+
+vector<int> permutationEquation(vector<int> p) {
+    vector<int> vals;
+
+    for (int i = 0; i < p.size(); i += 1) {
+        int val = p[i];
+        val = p[val-1];
+
+        vector<int>::iterator it = p.begin();
+        vector<int>::iterator end = p.end();
+        int c = 1;
+
+        while (it != end) {
+            if (*it == val) {
+                vals.push_back(c - 1); 
+                break;
+            }
+
+             c += 1;
+            it += 1;
+        }
+
+    }
+
+    return vals;
+}
 
 int main() {
+    vector<int> r = { 4 ,3 ,5 ,1 ,2 };
 
+    for (int a : permutationEquation(r)) {
+        cout << a << '\n';
+    }
   
     return 0;
 }
