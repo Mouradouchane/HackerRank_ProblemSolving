a3da6afb6fc1cf727ba436769e56ef23087d2a43
Mourad ouchane <mouradouchane@gmail.com>
2021-05-03 16:11:36 +0000

    solving CutS The sticks problem O(n*2)

--- HackerRank/CutTheSticks.cpp
+++ HackerRank/CutTheSticks.cpp
@@ -3,16 +3,24 @@
 #include <algorithm>
 
 // problem : https://www.hackerrank.com/challenges/cut-the-sticks/problem
-// solution O(n) ========================================================
+// solution O(n*2) ========================================================
 std::vector<int> cutTheSticks(std::vector<int> arr) {
-    int min = 0;
+        // when we store all cuts
     std::vector<int> cuts = { (int)arr.size() };
+        
+        // sorthing for good soultion O(n*2) , by using nice trick ;)
+    std::sort(arr.begin(), arr.end());  // O(n)
 
-    std::sort(arr.begin(), arr.end());
-    for (int i = 1; i < arr.size(); i += 1) {
+        // just for over all sticks in array , we start form stick '1' :)
+    for (int i = 1; i < arr.size(); i += 1) {  // O(n)
+
+        // if stick i != pervious stick that mean we need to make cuts & push new size
+        // but , when you use this trick we don't need to make any cuts
+        // just new size will be arr.size - i 
         if (arr[i] != arr[i - 1]) cuts.push_back(arr.size() - i);
     }
 
+        // return cuts 
     return cuts;
 }
 // ======================================================================
