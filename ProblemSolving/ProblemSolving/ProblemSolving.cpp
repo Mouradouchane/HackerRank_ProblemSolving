bb4d567544c82d74b4ef44a6c400dd7f229e81c7
Mourad ouchane <mouradouchane@gmail.com>
2021-05-04 00:58:44 +0000

    solving Equalize the Array O(n)

--- ProblemSolving/ProblemSolving/ProblemSolving.cpp
+++ ProblemSolving/ProblemSolving/ProblemSolving.cpp
@@ -5,39 +5,10 @@
 
 using namespace std;
 
-vector<int> permutationEquation(vector<int> p) {
-    vector<int> vals;
-
-    for (int i = 0; i < p.size(); i += 1) {
-        int val = p[i];
-        val = p[val-1];
-
-        vector<int>::iterator it = p.begin();
-        vector<int>::iterator end = p.end();
-        int c = 1;
-
-        while (it != end) {
-            if (*it == val) {
-                vals.push_back(c - 1); 
-                break;
-            }
-
-             c += 1;
-            it += 1;
-        }
-
-    }
-
-    return vals;
-}
-
 int main() {
-    vector<int> r = { 4 ,3 ,5 ,1 ,2 };
-
-    for (int a : permutationEquation(r)) {
-        cout << a << '\n';
-    }
+    vector<int> r = { 1 ,2 ,3 ,1 ,2 ,3 ,3 ,3 };
   
+
     return 0;
 }
 
