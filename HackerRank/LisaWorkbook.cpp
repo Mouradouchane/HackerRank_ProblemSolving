2708fb9493aff6aec18a8c1b5266b8b365908ca2
Mourad ouchane <mouradouchane@gmail.com>
2021-05-10 17:19:44 +0000

    solving lisa workbook problem O(n*k)

--- HackerRank/LisaWorkbook.cpp
+++ HackerRank/LisaWorkbook.cpp
@@ -1,30 +1,44 @@
-/*function workbook(n, k, chp) {
-    let special = 0;
+#include <bits/stdc++.h>
+using namespcae std;
 
-    let pagesize = k;
-    let pageindex = 1;
-  
-  for(let c = 0 ; c < chp.length ; c += 1){
-        //let arr =[];
+// problem : https://www.hackerrank.com/challenges/lisa-workbook/problem
+// solution O(n*k) =====================================================
+int workbook(int  n, int k, vector<int> chp) {
+	// total count of special problems 
+    int special = 0;
+	
+    // page size => just "k" how many problems per page we need it for turn page 
+    int pagesize = k;
+    // page index => for checking if problem is special or not "index == page index"
+    int pageindex = 1;
     
-    for(let problem = 1 ; problem <= chp[c] ;problem+=1){
-      if(problem == k+1){
-      //  arr.push("|");
+	// loop over all chapters :)
+    for(int c = 0 ; c < chp.size() ; c += 1){ // O(n)
+
+	// in each chapter we loop over all problems 1 => chop[c]
+        for(int problem = 1 ; problem <= chp[c] ; problem += 1){ // O(k)
+	    // if problem equal to k+1 that mean we need to turn page & increase k by pagesize
+            if(problem == k+1){
+		// increase by pagesize 
                 k += pagesize;
-        pageindex += 1;
-      }
-      
-      if(problem == pageindex){
-        special += 1;
-        //        arr.push("x");
-      }
-     // else arr.push(problem);
-    }
-    
-    k = pagesize;
-        pageindex += 1;
-      //console.log("chapter " , c+1 , " => ",arr.toString());
-    }
+		// turn page
+                pageindex += 1;
+            } 
+	    // if problem equal to pageindex that mean the problem special :) ;)
+            if(problem == pageindex){
+		// we add 1 to special counter
+                special += 1;
+            }
+        } // loop O(k) end
 
+	// in each time we to increase k 
+        k = pagesize;
+	// in each time we need to turn page
+	// to make sure not two chapters in same page 
+        pageindex +=1;
+    }
+	
+	// return special as last step
     return special;
-}*/
+}
+// =============================================================
