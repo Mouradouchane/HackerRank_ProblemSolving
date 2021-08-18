f001df3fc7ad568ba6c95d5537a6aee7a7a798a7
Mourad ouchane <mouradouchane@gmail.com>
2021-08-18 09:53:52 +0100

    new section data-structure section :=

--- ProblemSolving/ProblemSolving/ProblemSolving.cpp
+++ ProblemSolving/ProblemSolving/ProblemSolving.cpp
@@ -3,11 +3,61 @@
 #include <algorithm>
 #include <map>
 
-using namespace std;
+//using namespace std;
+
+template<typename t> class node {
+public :
+    node(){}
+    t value;
+    node* next = NULL;
+};
+
+template<typename t> class linkedList {
+public:
+    linkedList(){}
+    node<t>* first = NULL;
+    node<t>* last  = NULL;
+
+    void push_back(t value) {;
+        node<t>* newNode = new node<t>; 
+                 newNode->value = value;
+
+        if (first == NULL && last == NULL) {
+            first = newNode;
+            last = newNode;
+            first->next = last;
+            return;
+        }
+        else{
+            last->next = newNode;
+            last = newNode;
+        }
+    }
+
+    void showAll(){
+        node<t>* tempNode = first;
+  
+        while (tempNode != NULL){
+            std::cout << tempNode->value << std::endl;
+            tempNode = tempNode->next;
+        } 
+        
+    }
+};
+
 
 int main() {
-    vector<int> r = { 1 ,2 ,3 ,1 ,2 ,3 ,3 ,3 };
-  
+    
+    linkedList<int> firstOne;
+
+    firstOne.push_back(14);
+    firstOne.push_back(16);
+
+
+
+
+    firstOne.showAll();
+
 
     return 0;
 }
